42ead5a278ba4d410804c91810c1207652274a87
Zundal <ajs15010120@gmail.com>
2022-06-05 16:04:05 +0900

    postgresql 데이터베이스 조회

--- init.sql
+++ init.sql
@@ -1,3 +1,9 @@
 -- postgresql
 --  command -> \du 
 SELECT * FROM PG_USER;
+
+-- \list 또는 \l
+SELECT datname FROM pg_database -- 전체 데이터베이스 조회
+SELECT datname FROM pg_database WHERE datistemplate = false -- 사용자가 생성한 데이터베이스만 조회
+select nspname from pg_catalog.pg_namespace -- 현재 db의 전체 스키마 조회
+select tablename from pg_tables -- 전체 테이블 조회
