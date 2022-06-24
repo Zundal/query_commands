27492771175dcb8bc1aafa0198e057d3f56d8601
2O_OS <ajs15010120@gmail.com>
2022-06-24 22:06:15 +0900

    추가 : 테이블

--- basicUsertable.sql
+++ basicUsertable.sql
@@ -28,3 +28,35 @@ CREATE TABLE public.bord (
 
 -- 리테일 스토어 erd
 -- https://www.freeprojectz.com/entity-relationship/retail-store-management-system-er-diagram
+-- 이커머스 erd
+
+
+CREATE TABLE public.login (
+    login_id VARCHAR ( 100 ) PRIMARY KEY,
+    login_role_id VARCHAR ( 100 ),
+    login_username VARCHAR ( 100 ),
+    user_password VARCHAR ( 100 )
+);
+
+create table public.roles (
+    role_id varchar ( 100 ) primary key ,
+    role_name varchar ( 100 ),
+    role_desc varchar ( 255 )
+);
+
+create table public.payment (
+  pay_id varchar ( 100 ) primary key ,
+  pay_amt varchar ( 100 ),
+  pay_stu_id varchar ( 100 ),
+  pay_date date,
+  pay_desc varchar ( 255 )
+);
+
+create table public.product (
+    pro_id varchar ( 100 ) primary key ,
+    pro_num numeric,
+    pro_cus_id varchar ( 100 ),
+    pro_desc varchar ( 255 ),
+    pro_items varchar ( 255 ),
+    pro_type varchar ( 100 )
+);
